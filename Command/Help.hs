c115c025eef837bfb20cb0fb3e2aacf0a64c56b5
lauri <sphaso@gmail.com>
2014-11-10 19:44:43 +0100

    [m] Added gt-help to gt-help

--- Command/Help.hs
+++ Command/Help.hs
@@ -2,4 +2,5 @@ module Command.Help where
 
 gtHelp :: IO ()
 gtHelp = do 
+    putStrLn "gt-help | Gives a short description for each command"
     putStrLn "gt-init | Initializes a new gt/git repository"
