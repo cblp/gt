fa640535c5e8bcf6aa550b63d633200b09804980
lauri <sphaso@gmail.com>
2014-11-10 21:00:49 +0100

    [m] only one PutStr

--- Command/Help.hs
+++ Command/Help.hs
@@ -2,5 +2,5 @@ module Command.Help where
 
 gtHelp :: IO ()
 gtHelp = do 
-    putStrLn "gt-help | Gives a short description for each command"
-    putStrLn "gt-init | Initializes a new gt/git repository"
+    putStrLn "gt-help | Gives a short description for each command \n\
+             \gt-init | Initializes a new gt/git repository"
