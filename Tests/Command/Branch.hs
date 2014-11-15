3b798ce3b7ffdacb3f7fac55bf4aae6cca9734b1
Yuriy Syrovetskiy <cblp@cblp.su>
2014-11-15 21:06:19 +0300

    [+] test if no branch

--- Tests/Command/Branch.hs
+++ Tests/Command/Branch.hs
@@ -1,6 +1,8 @@
 import System.Directory ( setCurrentDirectory )
-import System.Exit      ( exitFailure, exitSuccess )
+import System.Exit      ( ExitCode(ExitSuccess), exitFailure, exitSuccess )
 import System.IO.Temp   ( withSystemTempDirectory )
+import System.Posix     ( setEnv )
+import System.Process   ( system )
 import Test.HUnit       ( Test(TestCase), assertEqual, failures, runTestTT )
 
 -- internal modules
@@ -10,12 +12,28 @@ import Command.Init     ( gtInit )
 
 main :: IO ()
 main = do
+    setEnv "GIT_EDITOR" "false" True
     testResult <- runTestTT $ TestCase $
         withSystemTempDirectory "" $ \tmp -> do
             setCurrentDirectory tmp
             gtInit
-            mbranch <- gtBranch
-            assertEqual "gtBranch" (Just "master") mbranch
+            gtBranch >>= assertEqual "gtBranch" (Just "master")
+            run "touch afile"
+            runGit "commit --allow-empty --message=message"
+            runGit "checkout $(git rev-parse HEAD)"
+            gtBranch >>= assertEqual "gtBranch" Nothing
     if failures testResult /= 0
         then exitFailure
         else exitSuccess
+
+
+run :: String -> IO ()
+run cmd = system cmd >>= assertEqual cmd ExitSuccess
+
+runGit :: String -> IO ()
+runGit cmd =
+    run $ "git "
+        ++ unwords ["-c " ++ k ++ "=" ++ v | (k, v) <- config]
+        ++ " "
+        ++ cmd
+    where config = [("user.email", "test@example.org")]
