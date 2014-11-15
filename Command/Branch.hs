2eb1a207d9079a5fa29db0fcaaf95226c59f821d
Alexander Putilin <avp-13@yandex.ru>
2014-11-15 16:57:27 +0300

    Return branch name instead of ref name

--- Command/Branch.hs
+++ Command/Branch.hs
@@ -1,17 +1,18 @@
 module Command.Branch where
 
-import Git          ( withRepository, RefName )
+import Git          ( withRepository)
 import Git.Libgit2  ( lgFactory )
 import Git.Types    ( lookupReference, RefTarget(..) )
+import qualified Data.Text as Text
 
 
-gtBranch :: IO (Maybe RefName)
-gtBranch = do
+gtBranch :: IO (Maybe String)
+gtBranch =
     withRepository lgFactory "." $ do
         ref <- lookupReference "HEAD"
         case ref of
             Nothing -> return Nothing
-            Just reftarget -> do
+            Just reftarget ->
                 case reftarget of
                     RefObj _ -> return Nothing
-                    RefSymbolic refname -> return $ Just refname
+                    RefSymbolic refname -> return $ Just $ Text.unpack $ last $ Text.splitOn "/" refname
--- Command/BranchMain.hs
+++ Command/BranchMain.hs
@@ -1,9 +1,8 @@
 import Command.Branch ( gtBranch )
-import qualified Data.Text as Text
 
 main :: IO ()
 main = do
-    refname <- gtBranch
-    case refname of
+    branch <- gtBranch
+    case branch of
         Nothing -> putStrLn "WAT. Weird HEAD link" 
-        Just name -> putStrLn $ Text.unpack $ last $ Text.splitOn "/" name
+        Just name -> putStrLn $ name
