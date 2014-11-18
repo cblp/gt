28a67b873ab5db67d5a27557057f0cac336e7ea0
Alexander Putilin <avp-13@yandex.ru>
2014-11-18 22:44:52 +0300

    Refactor out currentBranch function

--- Command/Branch.hs
+++ Command/Branch.hs
@@ -1,18 +1,22 @@
 module Command.Branch where
 
-import Git          ( withRepository)
+import Git          ( withRepository, MonadGit)
 import Git.Libgit2  ( lgFactory )
 import Git.Types    ( lookupReference, RefTarget(..) )
 import qualified Data.Text as Text
 
+currentBranch :: MonadGit repo monadGit => monadGit (Maybe String)
+currentBranch = do
+    ref <- lookupReference "HEAD"
+    case ref of
+        Nothing -> return Nothing
+        Just reftarget ->
+            case reftarget of
+                RefObj _ -> return Nothing
+                RefSymbolic refname -> return $ Just $ Text.unpack $ last $ Text.splitOn "/" refname
 
 gtBranch :: IO (Maybe String)
 gtBranch =
     withRepository lgFactory "." $ do
-        ref <- lookupReference "HEAD"
-        case ref of
-            Nothing -> return Nothing
-            Just reftarget ->
-                case reftarget of
-                    RefObj _ -> return Nothing
-                    RefSymbolic refname -> return $ Just $ Text.unpack $ last $ Text.splitOn "/" refname
+        branch <- currentBranch
+        return $ branch
